1372a6264be3e4f2badf1e7b96932c9a0f27a36a
KING PROJECT <textleech@gmail.com>
2025-03-14 11:30:50 +0530

    Update database.py

--- modules/database.py
+++ modules/database.py
@@ -23,3 +23,30 @@ class Database:
     def get_user(self, id):
         self.cursor.execute("SELECT * FROM users WHERE id = ?", (id,))
         return self.cursor.fetchone()
+
+import sqlite3
+
+class Database:
+    def __init__(self, db_file):
+        self.conn = sqlite3.connect(db_file)
+        self.cursor = self.conn.cursor()
+
+    def create_table(self):
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY,
+                username TEXT
+            );
+        """)
+        self.conn.commit()
+
+    def insert_user(self, id, username):
+        self.cursor.execute("INSERT INTO users (id, username) VALUES (?, ?)", (id, username))
+        self.conn.commit()
+
+    def get_user(self, id):
+        self.cursor.execute("SELECT * FROM users WHERE id = ?", (id,))
+        return self.cursor.fetchone()
+
+    def close_connection(self):
+        self.conn.close()
