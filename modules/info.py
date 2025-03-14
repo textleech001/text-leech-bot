cd93bda53fd54f41dcf63c20b0863c45bbdec449
KING PROJECT <textleech@gmail.com>
2025-03-14 21:09:31 +0530

    Update info.py

--- modules/info.py
+++ modules/info.py
@@ -1,5 +1,17 @@
-BOT_TOKEN = "7602834411:AAE3HuMjGKQBVveGyhZlEAY5cH6wEn3FaWE"
+#info.py
+BOT_TOKEN = "your-bot-token"
 BOT_USERNAME = "your-bot-username"
 API_ID = "your-api-id"
 API_HASH = "your-api-hash"
+
+info = {
+    "bot_token": BOT_TOKEN,
+    "bot_username": BOT_USERNAME,
+    "api_id": API_ID,
+    "api_hash": API_HASH,
+}
+
+
+
+
 
