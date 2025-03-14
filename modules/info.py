70becbfe072773dcce75aac0b6004c03660437ed
KING PROJECT <textleech@gmail.com>
2025-03-14 11:46:38 +0530

    Update info.py

--- modules/info.py
+++ modules/info.py
@@ -1,4 +1,4 @@
 BOT_TOKEN = ""     your-bot-token
-BOT_USERNAME = ""    your-bot-username
-API_ID = ""      your-api-id
-API_HASH = ""   your-api-hash
+BOT_USERNAME = "@GanduPagal_bot"    your-bot-username
+API_ID = "23680771"      your-api-id
+API_HASH = "0c58f3e3fecefc4a9d8e5bcf6968a106"   your-api-hash
