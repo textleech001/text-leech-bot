58c8ddc25f0215ce807558b8de1d31573e1c34f5
KING PROJECT <textleech@gmail.com>
2025-03-14 20:47:31 +0530

    Update database.py

--- modules/database.py
+++ modules/database.py
@@ -1,29 +1,5 @@
 # powerd by @kingproject24
 
-from database import Database
-
-class Database:
-    def __init__(self):
-        self.conn = sqlite3.connect("database.db")
-        self.cursor = self.conn.cursor()
-
-    def create_table(self):
-        self.cursor.execute("""
-            CREATE TABLE IF NOT EXISTS users (
-                id INTEGER PRIMARY KEY,
-                username TEXT
-            );
-        """)
-        self.conn.commit()
-
-    def insert_user(self, id, username):
-        self.cursor.execute("INSERT INTO users (id, username) VALUES (?, ?)", (id, username))
-        self.conn.commit()
-
-    def get_user(self, id):
-        self.cursor.execute("SELECT * FROM users WHERE id = ?", (id,))
-        return self.cursor.fetchone()
-
 import sqlite3
 
 class Database:
@@ -32,7 +8,7 @@ class Database:
         self.cursor = self.conn.cursor()
 
     def create_table(self):
-        self.cursor.execute("""
+        self.cursor.execute(""" 
             CREATE TABLE IF NOT EXISTS users (
                 id INTEGER PRIMARY KEY,
                 username TEXT
@@ -50,11 +26,3 @@ class Database:
 
     def close_connection(self):
         self.conn.close()
-
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-class Database:
-    def __init__(self):
-        pass
